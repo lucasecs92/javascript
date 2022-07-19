2dbf1d9539cc0aeea5e4da73b8d722db60e5872d
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-07-19 14:49:33 -0300

    condicionais pt01

--- curso em video/aula11/ex010.js
+++ curso em video/aula11/ex010.js
@@ -1,1 +1,20 @@
-console.log('O console funcionou corretamente!')
+// F8 para executar os codigos no console do node.js Exec
+
+console.log("O console funcionou corretamente!");
+
+// condição simples! Quando usa apenas if
+
+var vel1 = 60.5;
+console.log(`A velocidade do seu carro é ${vel1}km/h`);
+if (vel1 > 60) {
+  console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}
+
+
+// uso do else
+var vel2 = 50;
+if (vel2 > 70) {
+  console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+} else {
+  console.log("Dirija sempre usando cinto de segurança!");
+}
---
+++ curso em video/aula11/ex011.js
@@ -0,0 +1,18 @@
+// condição composta!
+
+var país = "EUA";
+console.log(`Vivendo em ${país}`);
+if (país == "Brasil") {
+  console.log("Você é Brasileiro!");
+} else {
+  console.log("Você é Estrangeiro!");
+}
+
+// invertido 
+var país = "Brasil";
+console.log(`Vivendo em ${país}`);
+if (país != "Brasil") { // não igual.. diferente de..
+  console.log("Você é Estrangeiro!");
+} else {
+  console.log("Você é Brasileiro!");
+}
