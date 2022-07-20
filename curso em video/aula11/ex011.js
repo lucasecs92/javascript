5ce84ce986a518df24561a3d14719d0595c8e378
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-07-20 11:02:09 -0300

    criando desafio

--- curso em video/aula11/ex011.js
+++ curso em video/aula11/ex011.js
@@ -8,10 +8,11 @@ if (país == "Brasil") {
   console.log("Você é Estrangeiro!");
 }
 
-// invertido 
+// invertido
 var país = "Brasil";
 console.log(`Vivendo em ${país}`);
-if (país != "Brasil") { // não igual.. diferente de..
+if (país != "Brasil") {
+  // não igual.. diferente de..
   console.log("Você é Estrangeiro!");
 } else {
   console.log("Você é Brasileiro!");
