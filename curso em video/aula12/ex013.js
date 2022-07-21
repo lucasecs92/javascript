86313bbea7f2704e72d4dc8f2acfe2026655f8e8
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-07-21 10:57:17 -0300

    condições pt2

--- curso em video/aula12/ex013.js
+++ curso em video/aula12/ex013.js
@@ -1,4 +1,30 @@
-var idade = 22
+/*
+var idade = 20;
 if (idade < 18) {
-    
+  console.log("Menor de idade");
+} else {
+  console.log("Maior de idade");
 }
+*/
+
+var age = 67;
+console.log(`Você tem ${age} anos.`)
+if (age < 16) {
+  console.log("Não vota");
+} else if (age < 18 || age > 65) {
+  // Quem tem idade acima de 16 e menor que 18 ou a idade acima de 65 o voto é opcional
+  console.log("Voto opcional");
+} else {
+  console.log("Voto obrigatório");
+}
+
+/*
+var age = 22;                                     var age = 22;
+if (age < 16) {                                   if (age < 16) {              
+  console.log("Não vota");                          console.log("Não vota");    
+} else {                                ou        } else if (age > 18) {
+    if (age > 18) {                                  console.log("Voto opcional");
+      console.log("Voto opcional");                 }
+  }                                               }
+}                                                 
+*/
