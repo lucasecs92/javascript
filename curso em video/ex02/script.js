69a5f523e0d6f018c6cc7df7b21404bfb8655501
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-07-25 11:01:25 -0300

    ex02

---
+++ curso em video/ex02/script.js
@@ -0,0 +1,6 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+}
