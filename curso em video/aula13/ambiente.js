2ff2906cf090c6ad446dda8150af28d9b7defc72
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-07-27 16:59:51 -0300

    while

---
+++ curso em video/aula13/ambiente.js
@@ -0,0 +1,13 @@
+// escrevendo na tela quantas vezes eu quiser
+var c = 1;
+while (c <= 5) {
+  console.log("Tudo bem?");
+  c++; // c = c + 1
+}
+
+// utilizando placeholder
+var s = 1;
+while (s <= 5) {
+  console.log(`Passo ${s}`);
+  s++;
+}
