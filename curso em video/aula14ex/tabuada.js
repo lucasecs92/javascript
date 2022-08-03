d78408bd52fecaa60107ff26bf74ff39e9e99794
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2022-08-03 15:33:37 -0300

    tabuada

---
+++ curso em video/aula14ex/tabuada.js
@@ -0,0 +1,13 @@
+function tabuada() {
+  let num = document.getElementById("txtn");
+  let tab = document.getElementById("seltab");
+  if (num.value.length == 0) { // se o num estiver vazio
+    window.alert('Por favor, digite um número!')
+  } else {
+    let n = Number(num.value);
+    let c = 1
+    while (c <= 10) {
+        re
+    }
+  }
+}
